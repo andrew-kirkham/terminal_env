795dd93b4e6e1fc22f7e247846c6b92a88e915d0
Andrew Kirkham <andrew.nicholas.kirkham@gmail.com>
2016-02-11 10:44:04 -0600

    git directory for updating can be overriden in bashrc_extra. gitignore for swp files

--- setup_scripts/updateAllRepos.bash
+++ setup_scripts/updateAllRepos.bash
@@ -1,10 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 #
 #Fetches updates for all git repositories located in a specified location
 #This will not work if your repository has spaces
-
-
-GIT_DIRECTORY=~/Documents/Repos/
 
 function update() {
     dirs=$(find $GIT_DIRECTORY -mindepth 1 -maxdepth 1 -type d)
@@ -14,6 +11,11 @@ function update() {
         git fetch -q --all > /dev/null 2>&1
         cd ..
     done
-    }
+}
+
+source ~/.bashrc_extra
+if [ -z "$GIT_DIRECTORY" ]; then
+    GIT_DIRECTORY=~/Documents/Repos/
+fi
 
 update
