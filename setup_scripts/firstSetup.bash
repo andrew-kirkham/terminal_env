d35b3a206e54629280749395570e35cb1b0ca856
Andrew Kirkham <akirkham@gogoair.com>
2017-03-09 11:21:07 -0600

    adding source line to profile for mac env

--- setup_scripts/firstSetup.bash
+++ setup_scripts/firstSetup.bash
@@ -6,6 +6,12 @@ mkdir ~/.vim/plugins
 mkdir ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 
+#if on macos, add source bashrc to profile so that it is sourced on every terminal open
+os=$(uname -s)
+if [[ os -eq "Darwin" ]]; then
+    echo 'source ~/.bashrc' >> ~/.profile
+fi
+
 #make the extra file
 touch ~/.bashrc_extra
 
